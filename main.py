d45bb8c0af62b35be1a8e3f639b6579dea67edd8
ahmedoff34 <emintiktok218@gmail.com>
2024-08-27 15:32:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
-from telegram import Bot
-from telegram import Update
+from telegram import Bot, Update
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
+
 # Bot Tokenini buraya ekle
 TOKEN = '7024230778:AAGTFI1s7RXz2LNro55NJIO6NVcKSZMwmR8'
 
@@ -18,40 +18,35 @@ class UserBot:
 user_bot = UserBot('SESSION_ID_1')
 
 # Botunuzu oluşturun
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
+app = ApplicationBuilder().token(TOKEN).build()
 
-def start(update: Update, context: CallbackContext):
+async def start(update: Update, context: CallbackContext):
     welcome_message = "Merhaba! Benimle iletişime geçmek için `/send` komutunu kullanabilirsiniz."
-    update.message.reply_text(welcome_message)
+    await update.message.reply_text(welcome_message)
 
-def send_to_users(update: Update, context: CallbackContext):
+async def send_to_users(update: Update, context: CallbackContext):
     message = ' '.join(context.args[1:])  # Komuttan sonra gelen mesajı al
     if not message:
-        update.message.reply_text('Mesaj sağlamalısınız.')
+        await update.message.reply_text('Mesaj sağlamalısınız.')
         return
 
     chat_id = update.message.chat_id
 
     try:
-        members = context.bot.get_chat_administrators(chat_id)
+        members = await context.bot.get_chat_administrators(chat_id)
         user_ids = [member.user.id for member in members if member.user.id != context.bot.id]
-        
+
         for user_id in user_ids:
             user_bot.send_message(user_id, message)  # Kullanıcı botu ile mesaj gönder
             time.sleep(2)  # Spam olmaması için 2 saniye bekleme süresi
     except Exception as e:
-        update.message.reply_text(f'Hata: {str(e)}')
+        await update.message.reply_text(f'Hata: {str(e)}')
 
 # Komutları ekle
-start_handler = CommandHandler('start', start)
-send_message_handler = CommandHandler('send', send_to_users)
-
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(send_message_handler)
+app.add_handler(CommandHandler('start', start))
+app.add_handler(CommandHandler('send', send_to_users))
 
 # Başlama logu
 print("Bot başlatıldı.")
 
-updater.start_polling()
-updater.idle()
+app.run_polling()
